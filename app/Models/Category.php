176c2aefe3fb0b6519c5d57820bc4f0dff5bb005
Diego Oliveira da Silva <diegolinkk@gmail.com>
2022-09-04 22:38:25 -0300

    adicionado relacionamento entre models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
 }
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
